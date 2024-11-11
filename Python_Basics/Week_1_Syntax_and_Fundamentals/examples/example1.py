09dcca6857961614d38984a871bdd6704ced7c55
Solomon Eshun <Ssolomoneinsteinmit300@gmail.com>
2024-11-11 22:44:49 +0000

    week1: example3

--- Python_Basics/Week_1_Syntax_and_Fundamentals/examples/example1.py
+++ Python_Basics/Week_1_Syntax_and_Fundamentals/examples/example1.py
@@ -1,1 +1,25 @@
 # Performing basic arithmetic
+a = 25
+b = 10
+
+add = a + b # 25 + 10
+multiplication = a * b
+subtraction = a - b
+power1 = a ** 2 # a to the power of 2 = 625
+power2 = b ** 3 # b to the power of 3
+division = a / b
+floor_division = a // b
+modulus = a % b
+
+print("a + b -> ", add)
+print("a * b -> ", multiplication)
+print("a - b -> ", subtraction)
+print("a to the power 2 -> ", power1)
+print("b to the power 3 -> ", power2)
+print("a / b -> ", division)
+print("a // b -> ", floor_division)
+print("a % b -> ", modulus)
+print("80 % 6 -> ", 80 % 6)
+
+
+
--- Python_Basics/Week_1_Syntax_and_Fundamentals/examples/example2.py
+++ Python_Basics/Week_1_Syntax_and_Fundamentals/examples/example2.py
@@ -1,1 +1,23 @@
 # Demonstrating input and output
+
+# Ask the user to enter their name
+name = input("Enter your name: ")
+age = input("How old are you? ")
+school = input("Which school did ou go? ")
+
+print("Hello", name, "you are", age, "years old.")
+print(name, " attended ", school)
+
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+result = num1 + num2
+print(result)
+
+
+
+
+
+
+
+
+
